d6978bec30c40d89d43919cd5f455ccf2c49ed2f
Kelvin Zhang <zhangxp1998@gmail.com>
2020-02-15 15:32:03 -0500

    Add helper method to TensorR

--- src/scala/tensor/ir/TensorDifferentiation.scala
+++ src/scala/tensor/ir/TensorDifferentiation.scala
@@ -9,113 +9,161 @@ trait Diff {
 }
 
 trait TensorDifferentiation extends TensorOps {
-  class TensorR[A: Manifest: Numeric](val x: Tensor[A], var d: Tensor[A]) extends Diff{
+
+  object TensorR {
+    def apply(dims: Seq[Int], fillVal: Float): TensorR[Float] = {
+      val tensor = Tensor.fill[Float](dims, fillVal)
+      TensorR(tensor)
+    }
+    def apply(x: Tensor[Float]): TensorR[Float] = {
+      new TensorR(x, Tensor.zero[Float](x.dims))
+    }
+    def apply(dims: Seq[Int], f: Rep[Int] => Float): TensorR[Float] = {
+      val tensor = Tensor[Float](dims)
+      tensor.mapInplaceWithFlatIdx((_, idx) => f(idx))
+      TensorR(tensor)
+    }
+    def grad(f: TensorR[Float] => TensorR[Float]@cps[Unit])(x: Tensor[Float]): Tensor[Float] = {
+      val z = new TensorR[Float](x, Tensor.zero[Float](x.dims))
+      reset({
+        val res = f(z)
+        res.d = Tensor.fill[Float](res.x.dims, 1)
+      })
+      z.d
+    }
+
+    def grad(f: (TensorR[Float], TensorR[Float]) => TensorR[Float]@cps[Unit])
+            (x: Tensor[Float], y: Tensor[Float]): (Tensor[Float], Tensor[Float]) = {
+      val z1 = new TensorR[Float](x, Tensor.zero[Float](x.dims))
+      val z2 = new TensorR[Float](y, Tensor.zero[Float](x.dims))
+      reset({
+        val res = f(z1, z2)
+        res.d = Tensor.fill[Float](res.x.dims, 1)
+      })
+      (z1.d, z2.d)
+    }
+  }
+
+  class TensorR[A: Manifest : Numeric](val x: Tensor[A], var d: Tensor[A]) extends Diff {
     // Single element broadcast operations
-    def +(that: Rep[A]): TensorR[A] @diff = shift { k: (TensorR[A] => Unit) =>
+    def +(that: Rep[A]): TensorR[A]@diff = shift { k: (TensorR[A] => Unit) =>
       val y = new TensorR((x + that): Tensor[A], Tensor.zero[A](x.dims))
       k(y)
       this.d += y.d
     }
-    def -(that: Rep[A]): TensorR[A] @diff = shift { k: (TensorR[A] => Unit) =>
+
+    def -(that: Rep[A]): TensorR[A]@diff = shift { k: (TensorR[A] => Unit) =>
       val y = new TensorR((x - that): Tensor[A], Tensor.zero[A](x.dims))
       k(y)
       this.d += y.d
     }
-    def *(that: Rep[A]): TensorR[A] @diff = shift { k: (TensorR[A] => Unit) =>
+
+    def *(that: Rep[A]): TensorR[A]@diff = shift { k: (TensorR[A] => Unit) =>
       val y = new TensorR(x * that, Tensor.zero[A](x.dims))
       k(y)
       this.d += y.d * that
     }
-    def /(that: Rep[A]): TensorR[A] @diff = shift { k: (TensorR[A] => Unit) =>
+
+    def /(that: Rep[A]): TensorR[A]@diff = shift { k: (TensorR[A] => Unit) =>
       val y = new TensorR(x / that, Tensor.zero[A](x.dims))
       k(y)
       this.d += y.d / that
     }
 
     // Tensor-Tensor element wise operations
-    def +(that: TensorR[A]): TensorR[A] @diff = shift { k: (TensorR[A] => Unit) =>
+    def +(that: TensorR[A]): TensorR[A]@diff = shift { k: (TensorR[A] => Unit) =>
       val y = new TensorR(x add that.x, Tensor.zero[A](x.dims))
       k(y)
       that.d += y.d
       this.d += y.d
     }
-    def -(that: TensorR[A]): TensorR[A] @diff = shift { k: (TensorR[A] => Unit) =>
+
+    def -(that: TensorR[A]): TensorR[A]@diff = shift { k: (TensorR[A] => Unit) =>
       val y = new TensorR(x sub that.x, Tensor.zero[A](x.dims))
       k(y)
       that.d -= y.d
       this.d += y.d
     }
-    def *(that: TensorR[A]): TensorR[A] @diff = shift { k: (TensorR[A] => Unit) =>
+
+    def *(that: TensorR[A]): TensorR[A]@diff = shift { k: (TensorR[A] => Unit) =>
       val y = new TensorR(x mul that.x, Tensor.zero[A](x.dims))
       k(y)
       that.d += y.d mul this.x
       this.d += y.d mul that.x
     }
-    def /(that: TensorR[A]): TensorR[A] @diff = shift { k: (TensorR[A] => Unit) =>
+
+    def /(that: TensorR[A]): TensorR[A]@diff = shift { k: (TensorR[A] => Unit) =>
       val y = new TensorR(x mul that.x, Tensor.zero[A](x.dims))
       k(y)
       that.d -= y.d mul this.x div (that.x mul that.x)
       this.d += y.d div that.x
     }
   }
+
 }
 
 object NumR {
   implicit def toNumR(x: Double): NumR = new NumR(x, 0)
+
   def apply(x: Double): NumR = toNumR(x)
-  def grad(f: NumR => NumR @cps[Unit])(x: Double): Double = {
+
+  def grad(f: NumR => NumR@cps[Unit])(x: Double): Double = {
     val z = new NumR(x, 0.0)
-    reset {f(z).d = 1.0}
+    reset {
+      f(z).d = 1.0
+    }
     z.d
   }
-  def grad(f: (NumR, NumR) => NumR @cps[Unit])(x: Double, y: Double): (Double, Double) = {
+
+  def grad(f: (NumR, NumR) => NumR@cps[Unit])(x: Double, y: Double): (Double, Double) = {
     val z1 = new NumR(x, 0.0)
     val z2 = new NumR(y, 0.0)
-    reset {f(z1, z2).d = 1.0}
+    reset {
+      f(z1, z2).d = 1.0
+    }
     (z1.d, z2.d)
   }
 }
 
 class NumR(val x: Double, var d: Double) extends Diff {
 
-  def +(that: NumR): NumR @diff = shift { k: (NumR => Unit) =>
-    val y = new NumR(x+that.x, 0)
+  def +(that: NumR): NumR@diff = shift { k: (NumR => Unit) =>
+    val y = new NumR(x + that.x, 0)
     k(y)
     that.d += y.d
     this.d += y.d
   }
 
-  def -(that: NumR): NumR @diff = shift { k: (NumR => Unit) =>
-    val y = new NumR(x-that.x, 0)
+  def -(that: NumR): NumR@diff = shift { k: (NumR => Unit) =>
+    val y = new NumR(x - that.x, 0)
     k(y)
     that.d -= y.d
     this.d += y.d
   }
 
-  def *(that: NumR): NumR @diff = shift { k: (NumR => Unit) =>
-    val y = new NumR(x*that.x, 0)
+  def *(that: NumR): NumR@diff = shift { k: (NumR => Unit) =>
+    val y = new NumR(x * that.x, 0)
     k(y)
     that.d += y.d * this.x
     this.d += y.d * that.x
   }
 
-  def /(that: NumR): NumR @diff = shift { k: (NumR => Unit) =>
-    val y = new NumR(x/that.x, 0)
+  def /(that: NumR): NumR@diff = shift { k: (NumR => Unit) =>
+    val y = new NumR(x / that.x, 0)
     k(y)
-    that.d -= y.d * this.x/(that.x*that.x)
+    that.d -= y.d * this.x / (that.x * that.x)
     this.d += y.d / that.x
   }
 }
 
 
-
-
 object TensorDifferentiation {
   def main(args: Array[String]): Unit = {
-    val dslDriver = new TensorDriverC[String,Unit] with TensorDifferentiation {
+    val dslDriver = new TensorDriverC[String, Unit] with TensorDifferentiation {
       override def snippet(x: Rep[String]): Rep[Unit] = {
         val x = Tensor.fill[Float](Seq(2), 1)
-        def grad(f: TensorR[Float] => TensorR[Float] @cps[Unit])(x: Tensor[Float]): Tensor[Float] = {
+
+        def grad(f: TensorR[Float] => TensorR[Float]@cps[Unit])(x: Tensor[Float]): Tensor[Float] = {
           val z = new TensorR[Float](x, Tensor.zero[Float](x.dims))
           reset({
             val res = f(z)
@@ -123,13 +171,15 @@ object TensorDifferentiation {
           })
           z.d
         }
-        val gradient = grad(a => a*a*a)(x)
+
+        val gradient = grad(a => a * a * a)(x)
         println(gradient(0))
         println(gradient(1))
       }
     }
 
 
-    dslDriver.eval("5")
+    val res = dslDriver.eval("5")
+    println(res.mkString("\n"))
   }
 }
