2767a931f336345804342a28cc41fe4dcb00ed55
Kelvin Zhang <zhangxp1998@gmail.com>
2020-02-26 09:18:34 -0500

    Fix bug in memory allocator

--- src/scala/tensor/ir/StagedMemoryAllocator.scala
+++ src/scala/tensor/ir/StagedMemoryAllocator.scala
@@ -16,9 +16,9 @@ object StagedMemoryAllocator {
   def allocate(events: Seq[MemoryEvent]): Map[Int, MemoryBlock] = {
     // From size to memory block
     val freelist = new mutable.TreeMap[Int, mutable.Set[MemoryBlock]]()
-    val maxsize = events.filter(_.isInstanceOf[Allocation]).map{case Allocation(_, size) => size}.sum
+    val maxsize = events.filter(_.isInstanceOf[Allocation]).map{case Allocation(_, size) => size.toLong}.sum
 
-    freelist.put(maxsize, mutable.Set(MemoryBlock(0, maxsize)))
+    freelist.put(maxsize.toInt, mutable.Set(MemoryBlock(0, maxsize.toInt)))
 
     var mem_used = 0;
     var min_mem = 0;
@@ -29,17 +29,18 @@ object StagedMemoryAllocator {
         val minSize = freelist.keys.find(s => s >= size).get
         val freeblocks = freelist(minSize)
         val block = freeblocks.head
+        freeblocks.remove(block)
+        if (freeblocks.isEmpty) freelist.remove(minSize)
         if (block.size == size) {
           allocationPlan(id) = block
-          freelist(minSize) = freeblocks.tail
         } else {
           allocationPlan(id) = MemoryBlock(block.begin, size)
           val remain = MemoryBlock(block.begin + size, block.size - size)
           val list = freelist.getOrElseUpdate(remain.size, mutable.Set())
-          list -= block
           list += remain
         }
         mem_used += size
+        assert(mem_used + freelist.values.flatMap(_.map(_.size)).sum == maxsize)
         min_mem = Math.max(mem_used, min_mem)
 
       case Deallocation(id, size, afterSym) =>
@@ -66,8 +67,9 @@ object StagedMemoryAllocator {
         }
         freelist.getOrElseUpdate(block.size, mutable.Set()).add(block)
         mem_used -= size
+        assert(mem_used + freelist.values.flatMap(_.map(_.size)).sum == maxsize)
     }
-    val lastBlk = allocationPlan.values.maxBy(_.begin)
+    val lastBlk = allocationPlan.values.maxBy(b => b.begin + b.size)
     println(s"Optimal: $min_mem Actual: ${lastBlk.begin + lastBlk.size}")
     allocationPlan.toMap
   }
