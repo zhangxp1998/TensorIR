9b06c7b936cf31bd985d7df4e8877ad7047eac75
Kelvin Zhang <zhangxp1998@gmail.com>
2020-03-14 19:00:54 -0400

    Add z3 based memory solver

--- src/scala/tensor/ir/MemorySolver.scala
+++ src/scala/tensor/ir/MemorySolver.scala
@@ -10,6 +10,48 @@ import tensor.ir.StagedMemoryAllocator.{Allocation, Deallocation, MemoryBlock, M
 import scala.collection.mutable
 
 object MemorySolver {
+  def allocate(events: Seq[MemoryEvent]): Unit /*Map[Int, MemoryBlock]*/ = {
+//    val maxsize = events.filter(_.isInstanceOf[Allocation]).map{case Allocation(_, size) => size}.sum
+    val activeBlocks = new mutable.TreeSet[Int]()
+    val allocationSizes: Map[Int, Int] =
+      events.filter(_.isInstanceOf[Allocation]).map{case Allocation(id, size) => (id, size)}.toMap[Int, Int]
+
+    var lower_bound = 0
+    var sum = 0
+    val fileName = "memory.z3"
+    val writer = new PrintWriter(fileName)
+    val cap = "cap"
+    try {
+      writer.println("(define-fun max ((x Int) (y Int)) Int\n  (ite (< x y) y x))")
+      writer.println(s"(declare-const $cap Int)")
+      events.foreach {
+        case Allocation(id, size) =>
+          val x = "x_" + id
+          writer.println(s"(declare-const $x Int)")
+          writer.println(s"(assert (>= $x 0))")
+          writer.println(s"(assert (>= $cap (+ $x $size)))")
+          activeBlocks.foreach(y_id => {
+            val y = "x_" + y_id
+            writer.println(s"(assert (or (<= (+ $x $size) $y) (<= (+ $y ${allocationSizes(y_id)}) $x) ))")
+          })
+          activeBlocks += id
+          sum += size
+          lower_bound = math.max(lower_bound, sum)
+        case Deallocation(id, size, afterSym) =>
+          activeBlocks -= id
+          sum -= size
+      }
+      writer.println(s"(minimize $cap)")
+      writer.println("(check-sat)")
+      writer.println("(get-model)")
+      writer.println("(get-objectives)")
+      writer.flush()
+      import scala.sys.process._
+      (s"z3 $fileName": ProcessBuilder).lineStream.foreach(println)
+    } finally {
+      writer.close()
+    }
+  }
   def solve(events: Seq[MemoryEvent]): Map[Int, MemoryBlock] = {
     implicit val model = MPModel(SolverLib.oJSolver)
     val maxsize = events.filter(_.isInstanceOf[Allocation]).map{case Allocation(_, size) => size}.sum
