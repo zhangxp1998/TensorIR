0476235156964b271cc803f1f6d45f5ff6a59e5b
Kelvin Zhang <zhangxp1998@gmail.com>
2020-04-12 15:58:04 -0400

    Add randFloat with range

--- src/scala/tensor/ir/RandomOps.scala
+++ src/scala/tensor/ir/RandomOps.scala
@@ -8,6 +8,10 @@ import lms.macros.SourceContext
 
 import scala.collection.mutable
 
+// A type that maps to C++ std::uniform_real_distribution<> real_dis{a, b};
+trait UniformFloatDistribution {
+}
+
 trait RandomOps extends Base {
   val RAND = Backend.Const("random")
   def randInt(): Rep[Int] = {
@@ -16,6 +20,16 @@ trait RandomOps extends Base {
   // Random number in range [0.0f, 1.0f]
   def randFloat(): Rep[Float] = {
     Wrap[Float](Adapter.g.reflectEffect("rand_float")(RAND)(RAND))
+  }
+  def sampleDistribution(dis: Rep[UniformFloatDistribution]): Rep[Float] = {
+    Wrap[Float](Adapter.g.reflectEffect("rand_sample", Unwrap(dis))(RAND)(RAND))
+  }
+  def getUniformFloatDistribution(lower: Float, upper: Float): Rep[UniformFloatDistribution] = {
+    Wrap[UniformFloatDistribution](Adapter.g.reflect("rand_create_distribution", Backend.Const((lower, upper))))
+  }
+  def randFloat(a: Float, b: Float): Rep[Float] = {
+    val distribution = getUniformFloatDistribution(a, b)
+    sampleDistribution(distribution)
   }
 }
 
@@ -31,11 +45,21 @@ trait RandomOpsCodegen extends CGenBase {
         |std::uniform_real_distribution<> real_dis{0.0, 1.0};
         |""".stripMargin)
   }
+
+  override def remap(m: Manifest[_]): String = m.toString() match {
+    case s if s.contains("UniformFloatDistribution") => "std::uniform_real_distribution<float>"
+    case _ => super.remap(m)
+  }
   override def shallow(n: Node): Unit = n match {
     case Node(s, "rand_int", _, _) =>
-      emit("int_dis(rng")
+      emit("int_dis(rng)")
     case Node(s, "rand_float", _, _) =>
       emit("real_dis(rng)")
+    case Node(s, "rand_create_distribution", Const((a: Float, b: Float)):: Nil, _) =>
+      emit(s"std::uniform_real_distribution<float>{$a, $b}")
+    case Node(s, "rand_sample", dis::Nil, _) =>
+      shallow(dis)
+      emit("(rng)")
     case _ => super.shallow(n)
   }
 }
