989fd120e3fc2b06a64a51cfae8e0e52f5caa1cf
Kelvin Zhang <zhangxp1998@gmail.com>
2020-04-30 18:08:48 -0400

    Correctly implement GPU matmul backprop

--- gen/gpu_tensor.cpp
+++ gen/gpu_tensor.cpp
@@ -9,17 +9,37 @@ cublasHandle_t gpu::createCublasHandle() {
     return handle;
 }
 
+static void sgemm_column_major(const char transA, const char transB, const float *a, const float *b,
+           float *c, const size_t M, const size_t K, const size_t N,
+           const float& alpha, const float& beta) {
+  
+  int lda = tolower(transA) == 'n' ? M : K;
+  int ldb = tolower(transB) == 'n' ? K : N;
+  int ldc = M;
+  auto&& opA = tolower(transA) == 'n' ? CUBLAS_OP_N : CUBLAS_OP_T;
+  auto&& opB = tolower(transB) == 'n' ? CUBLAS_OP_N : CUBLAS_OP_T;
+  auto error = cublasSgemm(gpu::cublasHandle, opA, opB, M, N, K, &alpha, a, lda, b, ldb, &beta, c, ldc);
+  assert(error == CUBLAS_STATUS_SUCCESS);
+}
+
+static char invertTrans(const char transA) {
+  return tolower(transA) == 'n' ? 'T' : 'N';
+}
+
 // Expects data in row major format
 void gpu::sgemm(const char transA, const char transB, const float *a, const float *b,
            float *c, const size_t M, const size_t K, const size_t N,
            const float& alpha, const float& beta) {
 
-  int64_t lda = tolower(transA) == 'n' ? K : M;
-  int64_t ldb = tolower(transB) == 'n' ? N : K;
-  int64_t ldc = N;
+  sgemm_column_major(invertTrans(transA), invertTrans(transB), b, a, c, N, K, M, alpha, beta);
+}
 
-  auto&& opA = tolower(transA) == 'n' ? CUBLAS_OP_T : CUBLAS_OP_N;
-  auto&& opB = tolower(transB) == 'n' ? CUBLAS_OP_T : CUBLAS_OP_N;
-  // CUBLAS expects data in column major format
-  cublasSgemm(cublasHandle, opA, opB, M, N, K, &alpha, a, lda, b, ldb, &beta, c, ldc);
+void gpu::matmul_backprop(const float *m1, const float *m2, const float *y,
+                     float *d1, float *d2, const size_t M, const size_t K,
+                     const size_t N) {
+  // m1: M*K, m2: K*N, y: M*N
+  // d1 += y * m2.T => M*N x N*K = M*K
+  // d2 += m1.T * y => K*M x M*N = K*N
+  gpu::sgemm('N', 'T', y, m2, d1, M, N, K, 1.0f, 1.0f);
+  gpu::sgemm('T', 'N', m1, y, d2, K, M, N, 1.0f, 1.0f);
 }
--- gen/gpu_tensor.h
+++ gen/gpu_tensor.h
@@ -8,34 +8,41 @@
 #include <thrust/device_ptr.h>
 #include <cublas_v2.h>
 
-#define checkCUDNN(expression)                                                 \
-  {                                                                            \
-    cudnnStatus_t status = (expression);                                       \
-    if (status != CUDNN_STATUS_SUCCESS) {                                      \
-      std::cerr << "Error on line " << __LINE__ << ": "                        \
-                << cudnnGetErrorString(status) << "\n";                        \
-      std::exit(EXIT_FAILURE);                                                 \
-    }                                                                          \
+#define checkCUDNN(expression)                          \
+  {                                                     \
+    cudnnStatus_t status = (expression);                \
+    if (status != CUDNN_STATUS_SUCCESS)                 \
+    {                                                   \
+      std::cerr << "Error on line " << __LINE__ << ": " \
+                << cudnnGetErrorString(status) << "\n"; \
+      std::exit(EXIT_FAILURE);                          \
+    }                                                   \
   }
 
-namespace gpu {
+namespace gpu
+{
 template <typename T>
-T *gpu_malloc(size_t size) {
+T *gpu_malloc(size_t size)
+{
   void *memory{nullptr};
   auto error = cudaMalloc(&memory, size * sizeof(T));
   assert(error == cudaSuccess);
-  return static_cast<T*>(memory);
+  return static_cast<T *>(memory);
 }
 
-template <typename T> T read_gpu_mem(T *gpu_mem, size_t idx) {
+template <typename T>
+T read_gpu_mem(T *gpu_mem, size_t idx)
+{
   T val{};
-  auto error = cudaMemcpy(&val, gpu_mem, sizeof(T), cudaMemcpyDeviceToHost);
+  auto error = cudaMemcpy(&val, gpu_mem+idx, sizeof(T), cudaMemcpyDeviceToHost);
   assert(error == cudaSuccess);
   return val;
 }
 
-template <typename T> void write_gpu_mem(T *gpu_mem, size_t idx, T val) {
-  auto error = cudaMemcpy(gpu_mem, &val, sizeof(T), cudaMemcpyHostToDevice);
+template <typename T>
+void write_gpu_mem(T *gpu_mem, size_t idx, T val)
+{
+  auto error = cudaMemcpy(gpu_mem+idx, &val, sizeof(T), cudaMemcpyHostToDevice);
   assert(error == cudaSuccess);
 }
 
@@ -46,26 +53,32 @@ template <typename T> void write_gpu_mem(T *gpu_mem, size_t idx, T val) {
 //     *i = fillVal;
 // }
 
-template <typename T> void fill(T *begin, T *end, T fillVal) {
-    thrust::fill(thrust::device_ptr<T>(begin), thrust::device_ptr<T>(end), fillVal);
+template <typename T>
+void fill(T *begin, T *end, T fillVal)
+{
+  thrust::fill(thrust::device_ptr<T>(begin), thrust::device_ptr<T>(end), fillVal);
 }
 
-
-template <typename T, typename Callable> void transform(T *begin, T *end, T *dst, Callable func) {
-    thrust::transform(thrust::device_ptr<T>(begin), thrust::device_ptr<T>(end), thrust::device_ptr<T>(dst), std::move(func));
+template <typename T, typename Callable>
+void transform(T *begin, T *end, T *dst, Callable func)
+{
+  thrust::transform(thrust::device_ptr<T>(begin), thrust::device_ptr<T>(end), thrust::device_ptr<T>(dst), std::move(func));
 }
 
-template <typename T, typename Callable> void transform(T *begin, T *end, T* begin2, T *dst, Callable func) {
-    using thrust::device_ptr;
-    thrust::transform(device_ptr<T>(begin), device_ptr<T>(end), device_ptr<T>(begin2), device_ptr<T>(dst), std::move(func));
+template <typename T, typename Callable>
+void transform(T *begin, T *end, T *begin2, T *dst, Callable func)
+{
+  using thrust::device_ptr;
+  thrust::transform(device_ptr<T>(begin), device_ptr<T>(end), device_ptr<T>(begin2), device_ptr<T>(dst), std::move(func));
 }
 
 template <typename T>
-T *memdup(const T *src, size_t size) {
-    T* dst = gpu_malloc<T>(size);
-    auto error = cudaMemcpy(dst, src, size * sizeof(T), cudaMemcpyDeviceToDevice);
-    assert(error == cudaSuccess);
-    return dst;
+T *memdup(const T *src, size_t size)
+{
+  T *dst = gpu_malloc<T>(size);
+  auto error = cudaMemcpy(dst, src, size * sizeof(T), cudaMemcpyDeviceToDevice);
+  assert(error == cudaSuccess);
+  return dst;
 }
 
 cublasHandle_t createCublasHandle();
@@ -75,9 +88,13 @@ extern cublasHandle_t cublasHandle;
 // Expects data in row major format
 void sgemm(const char transA, const char transB, const float *a, const float *b,
            float *c, const size_t M, const size_t K, const size_t N,
-           const float& alpha, const float& beta);
+           const float &alpha, const float &beta);
 
+
+
+void matmul_backprop(const float *m1, const float *m2, const float *y,
+                     float *d1, float *d2, const size_t M, const size_t K,
+                     const size_t N);
 
 } // namespace gpu
-
 #endif
